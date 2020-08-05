fb6ea5340aa84576aa3f8cb9c76ca357671ad89f
SuiFengLiuNian <1329905627@qq.com>
2020-08-05 13:39:10 +0800

    Add files via upload
    修改播放器内容

---
+++ SFVideoCell.h
@@ -0,0 +1,22 @@
+//
+//  SFVideoCell.h
+//  SFPlayer
+//
+//  Created by apple on 2020/8/3.
+//  Copyright © 2020 随风流年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SFVideoModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SFVideoCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *videoImageView;
+@property (nonatomic, strong)   SFVideoModel        *videoModel;
+@property (weak, nonatomic) IBOutlet UILabel *titleLbl;
+@property (weak, nonatomic) IBOutlet UILabel *count;
+
+@end
+
+NS_ASSUME_NONNULL_END
